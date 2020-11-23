e4ad96639eb4082234b123638c677fccfc4c2bda
aurianer <aurianer@cscs.ch>
2020-11-23 13:04:02 +0100

    Add json print utility in the testing module

--- libs/core/testing/include/hpx/modules/testing.hpp
+++ libs/core/testing/include/hpx/modules/testing.hpp
@@ -1,4 +1,5 @@
 ////////////////////////////////////////////////////////////////////////////////
+//  Copyright (c) 2020 ETH Zurich
 //  Copyright (c) 2011 Bryce Lelbach
 //
 //  SPDX-License-Identifier: BSL-1.0
@@ -19,11 +20,14 @@
 #include <hpx/util/ios_flags_saver.hpp>
 
 #include <atomic>
+#include <chrono>
 #include <cstddef>
 #include <cstdint>
 #include <iostream>
+#include <map>
 #include <mutex>
 #include <sstream>
+#include <vector>
 
 namespace hpx { namespace util {
 
@@ -175,6 +179,61 @@ namespace hpx { namespace util {
 
         HPX_CORE_EXPORT extern fixture global_fixture;
 
+        // Json output for performance reports
+        class json_perf_times {
+
+            using key_t = std::tuple<std::string, std::string>;
+            using value_t = std::vector<double>;
+            using map_t = std::map<key_t, value_t>;
+
+            map_t m_map;
+
+            friend std::ostream &operator<<(std::ostream &strm, json_perf_times
+                    const &obj) {
+                strm << "{\n";
+                strm << "  \"outputs\" : [";
+                int outputs = 0;
+                for (auto &&item : obj.m_map) {
+                    if (outputs)
+                        strm << ",";
+                    strm << "\n    {\n";
+                    strm << "      \"name\" : \"" << std::get<0>(item.first) << "\",\n";
+                    strm << "      \"executor\" : \"" << std::get<1>(item.first) << "\",\n";
+                    strm << "      \"series\" : [";
+                    int series = 0;
+                    for (auto val : item.second) {
+                        if (series)
+                            strm << ", ";
+                        strm << val;
+                        ++series;
+                    }
+                    strm << "]\n";
+                    strm << "    }";
+                    ++outputs;
+                }
+                if (outputs)
+                    strm << "\n  ";
+                strm << "]\n";
+                strm << "}\n";
+                return strm;
+            }
+
+            public:
+                void add(std::string const &name, std::string const &executor,
+                        double time) {
+                    m_map[key_t(name, executor)].push_back(time);
+                }
+        };
+
+        json_perf_times &times() {
+            static json_perf_times res;
+            return res;
+        }
+
+        // Add time to the map for performance report
+        void add_time(std::string const &test_name, std::string const &executor,
+                double time);
+
     }    // namespace detail
 
     ////////////////////////////////////////////////////////////////////////////
@@ -182,6 +241,11 @@ namespace hpx { namespace util {
     HPX_CORE_EXPORT void print_cdash_timing(const char* name, double time);
     HPX_CORE_EXPORT void print_cdash_timing(
         const char* name, std::uint64_t time);
+
+    //template <typename TestSignature,
+    //         std::enable_if_t<std::is_function<TestSignature>::value>>
+    void perf_test_report(std::string const &name, std::string const
+            &exec, const std::size_t steps, function_nonser<void(void)> &&test);
 }}    // namespace hpx::util
 
 ////////////////////////////////////////////////////////////////////////////////
