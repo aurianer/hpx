60cb6a169e3490ddb32595454bbd38ca8c0b02ba
Auriane R <aurianer@cscs.ch>
2021-06-08 11:29:13 +0200

    Add failure test for send_bcast

--- libs/full/async_mpi/tests/unit/algorithm_send_bcast.cpp
+++ libs/full/async_mpi/tests/unit/algorithm_send_bcast.cpp
@@ -10,6 +10,8 @@
 #include <hpx/modules/execution.hpp>
 #include <hpx/modules/testing.hpp>
 
+#include <hpx/execution/tests/algorithm_test_utils.hpp>
+
 #include <mpi.h>
 
 namespace ex = hpx::execution::experimental;
@@ -41,6 +43,22 @@ int hpx_main()
             {
                 HPX_TEST_EQ(data, 42);
             }
+        }
+
+        // Failure path
+        {
+            bool exception_thrown = false;
+            try
+            {
+                mpi::send_bcast(error_sender{}) | ex::sync_wait();
+                HPX_TEST(false);
+            }
+            catch (std::runtime_error const& e)
+            {
+                HPX_TEST_EQ(std::string(e.what()), std::string("error"));
+                exception_thrown = true;
+            }
+            HPX_TEST(exception_thrown);
         }
 
         // let the user polling go out of scope
